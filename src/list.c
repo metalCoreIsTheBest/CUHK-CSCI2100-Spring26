b396eab1eb5b3245834083b07544fa7f84f96691
L.J.wsl <xxxx@xxx.com>
2026-02-08 15:46:53 +0800

    Update list implementation

--- src/list.c
+++ src/list.c
@@ -1,36 +1,86 @@
+// version 2.0
+// use linked list to implement
+
 #warning This implementation is from the lecture slides and may cause memory leakage problems
 
 #include <stdlib.h>
 
 #include "../include/list.h"
 
-// get List length recursively
-int ListLength(listADT L1) {
-    if (ListIsEmpty(L1))
-        return 0;
-    else
-        return 1 + ListLength(Tail(L1));
+struct listCDT {
+    listElementT h;
+    listADT t;
+};
+
+listADT EmptyList(void) {
+    return NULL;
 }
 
-listElementT SumList(listADT L1) {
-    if (ListIsEmpty(L1))
-        return 0;
-    else
-        return Head(L1) + SumList(Tail(L1));
+// returns a new list
+listADT Cons(listElementT h1, listADT t1) {
+    listADT list = malloc(sizeof(*list));
+
+    list->h = h1;
+    list->t = t1;
+
+    return list;
 }
 
-listElementT LastElement(listADT L1) {
-    if (ListIsEmpty(L1))
+listElementT Head(listADT list) {
+    if (ListIsEmpty(list)) {
         exit(EXIT_FAILURE);
-    else if (ListIsEmpty(Tail(L1)))
-        return Head(L1);
-    else
-        return LastElement(Tail(L1));
+    }
+
+    return list->h;
 }
 
-listADT Append(listADT L1, listElementT x) {
-    if (ListIsEmpty(L1))
+// return a new list containing the "tails"
+listADT Tail(listADT list) {
+    if (ListIsEmpty(list)) {
+        exit(EXIT_FAILURE);
+    }
+
+    return list->t;
+}
+
+int ListIsEmpty(listADT list) {
+    return list == NULL;
+}
+
+// get List length recursively
+int ListLength(listADT list) {
+    if (ListIsEmpty(list))
+        return 0;
+    else
+        return 1 + ListLength(Tail(list));
+}
+
+listElementT SumList(listADT list) {
+    if (ListIsEmpty(list))
+        return 0;
+    else
+        return Head(list) + SumList(Tail(list));
+}
+
+listElementT LastElement(listADT list) {
+    if (ListIsEmpty(list))
+        exit(EXIT_FAILURE);
+    else if (ListIsEmpty(Tail(list)))
+        return Head(list);
+    else
+        return LastElement(Tail(list));
+}
+
+listADT Append(listADT list, listElementT x) {
+    if (ListIsEmpty(list))
         return Cons(x, EmptyList());
     else
-        return Cons(Head(L1), Append(Tail(L1), x));
+        return Cons(Head(list), Append(Tail(list), x));
+}
+
+listADT Reverse(listADT list) {
+    if (ListIsEmpty(list))
+        return EmptyList();
+    else
+        return Append(Reverse(Tail(list)), Head(list));
 }
